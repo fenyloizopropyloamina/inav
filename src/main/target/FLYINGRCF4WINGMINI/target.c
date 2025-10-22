7c0afc26b02d19d553cebb71da73ed1d079df11d
fenyloizopropyloamina <mbotwina4@gmail.com>
2025-10-22 07:34:26 +0200

    Update target.c

--- src/main/target/FLYINGRCF4WINGMINI/target.c
+++ src/main/target/FLYINGRCF4WINGMINI/target.c
@@ -1,11 +1,29 @@
+/*
+ * This file is part of INAV.
+ *
+ * INAV is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * INAV is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with INAV.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <stdbool.h>
 #include <platform.h>
+
 #include "drivers/bus.h"
 #include "drivers/io.h"
 #include "drivers/pwm_mapping.h"
 #include "drivers/timer.h"
 #include "drivers/sensor.h"
 #include "drivers/timer_def_stm32f4xx.h"
-
 
 timerHardware_t timerHardware[] = {
     DEF_TIM(TIM8,  CH4,  PC9,  TIM_USE_OUTPUT_AUTO,   0, 0), // S1 D(2,7,7) UP217
